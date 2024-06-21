7765a54c1ed494bbfbebffd81c367a160f485ae9
sulosp <59638018+sulosp@users.noreply.github.com>
2024-06-22 01:50:45 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/sulosp.github.io/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
