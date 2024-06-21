b04b00052f8f3d1d534d7a72bfb279ced48f2782
sulosp <59638018+sulosp@users.noreply.github.com>
2024-06-22 01:59:02 +0530

    gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/sulosp.github.io/' : '',
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
+  
