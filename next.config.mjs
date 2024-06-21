ec8d80d25bb37fa48cb17f09c8841697382897a1
sulosp <59638018+sulosp@users.noreply.github.com>
2024-06-22 03:03:16 +0530

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,9 @@
 const nextConfig = {
     output: "export",
     reactStrictMode: true,  // <=== enables static exports
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/sulosp.github.io/' : '',
-    images: {
-      unoptimized: true,
-    },
   };
   
   export default nextConfig;
+  
+
   
