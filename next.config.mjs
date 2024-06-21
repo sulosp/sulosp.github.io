4be8afcdc02ffcbe3246f42ab3fc6b089b1c5b21
sulosp <59638018+sulosp@users.noreply.github.com>
2024-06-22 03:08:25 +0530

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    reactStrictMode: true,  // <=== enables static exports
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
 
-  
+export default nextConfig;
